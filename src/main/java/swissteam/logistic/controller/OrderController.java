e6cba9003a0f2b1715b0b48c3baf05a34b8980e1
eldaroo <darioegea@gmail.com>
2023-03-08 10:12:11 -0300

    use Gson library to always return a jsonObject

--- src/main/java/swissteam/logistic/controller/OrderController.java
+++ src/main/java/swissteam/logistic/controller/OrderController.java
@@ -8,14 +8,14 @@ import org.springframework.web.bind.annotation.*;
 import swissteam.logistic.exception.ApiRequestException;
 import swissteam.logistic.model.OrderModel;
 import swissteam.logistic.service.impl.OrderServiceImpl;
-
-import java.util.List;
+import com.google.gson.Gson;
 
 @RestController
 @CrossOrigin
 public class OrderController {
 
     private final OrderServiceImpl service;
+    private static final Gson gson = new Gson();
 
     public OrderController(OrderServiceImpl service) {
         this.service = service;
@@ -26,7 +26,7 @@ public class OrderController {
         try {
             return new ResponseEntity<>(service.getOrders(), HttpStatus.ACCEPTED);
         } catch (ApiRequestException e) {
-            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+            return new ResponseEntity<>(e.getErrorMessage(), e.getHttpStatus());
         }
     }
 
@@ -35,7 +35,7 @@ public class OrderController {
         try {
             return new ResponseEntity<>(service.getOrderById(id), HttpStatus.ACCEPTED);
         } catch (ApiRequestException e) {
-            return new ResponseEntity<>(e.getMessage(), e.getHttpStatus());
+            return new ResponseEntity<>(gson.toJson(e.getErrorMessage()), e.getHttpStatus());
 
         }
     }
@@ -51,9 +51,9 @@ public class OrderController {
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
         try {
             service.remove(id);
-            return new ResponseEntity<>("La orden "+id+" fue eliminada con éxito", httpHeaders, HttpStatus.ACCEPTED);
+            return new ResponseEntity<>(gson.toJson("La orden " + id + " fue eliminada con éxito"), httpHeaders, HttpStatus.ACCEPTED);
         } catch (ApiRequestException e) {
-            return new ResponseEntity<>(e.getMessage(), httpHeaders, e.getHttpStatus());
+            return new ResponseEntity<>(e.getErrorMessage(), httpHeaders, e.getHttpStatus());
         }
     }
 }
--- src/main/java/swissteam/logistic/exception/ApiRequestException.java
+++ src/main/java/swissteam/logistic/exception/ApiRequestException.java
@@ -1,17 +1,22 @@
 package swissteam.logistic.exception;
 
 import org.springframework.http.HttpStatus;
+import com.google.gson.Gson;
 
 public class ApiRequestException extends RuntimeException{
+
     private final HttpStatus httpStatus;
+    private static final Gson gson = new Gson();
 
     public ApiRequestException(String message, HttpStatus httpStatus) {
         super(message);
         this.httpStatus = httpStatus;
     }
-
     public HttpStatus getHttpStatus() {
         return httpStatus;
     }
 
+    public Object getErrorMessage() {
+        return gson.toJson(getMessage());
+    }
 }
