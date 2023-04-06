c7c9efb66c24adb851faaf20e23e35b218575f5a
eldaroo <darioegea@gmail.com>
2023-04-06 14:02:39 -0300

    create and config jwt in order to handle tokens y authentication

--- src/main/java/swissteam/logistic/LogisticApplication.java
+++ src/main/java/swissteam/logistic/LogisticApplication.java
@@ -3,17 +3,18 @@ package swissteam.logistic;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class LogisticApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LogisticApplication.class, args);
 	}
-
 
 	@Bean
 	CommandLineRunner commandLineRunner(PasswordEncoder encoder){
